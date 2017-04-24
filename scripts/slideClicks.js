00dcbd9ef9da6165366bc504e1089997cb737d18
WILL MEIER <willmeierart@gmail.com>
2017-04-23 20:58:50 -0600

    lightning talk final

---
+++ scripts/slideClicks.js
@@ -0,0 +1,13 @@
+$('.slide2bg').click(function(){
+    $('#deepSitePainting').toggle();
+    $('#painting').toggle();
+    });
+
+$('.slide4').click(function(){
+  $('#chessFill').toggle();
+  $('.largeGlass').toggle();
+  });
+
+$('.slide5').click(function(){
+  $('#bush').fadeToggle(10000);
+});
